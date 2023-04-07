c9e81ce79e4a8b04d67a02b2bd9b28e162ad5683
LaboRecydis <122175049+LaboRecydis@users.noreply.github.com>
2023-04-07 11:10:56 +0200

    Update process.py

--- process.py
+++ process.py
@@ -29,10 +29,7 @@ if __name__=="__main__":
     # Display the LOGO
     img1 = Image.open("IMG_PAPREC.jpg")
     img2 = Image.open("IMG_RECYDIS.jfif") 
-    img3= Image.open("echantillon.jpg")
-    img4= Image.open("procedure.jpg")
-    img5= Image.open("réactifs.jpg")
-    img6= Image.open("resultat.jpg")
+    
     st.sidebar.image(img1, width=250)
     st.sidebar.image(img2, width=250)
 
